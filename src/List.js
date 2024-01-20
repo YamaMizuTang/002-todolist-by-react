40c990cc34212eed85edb9847da335cbf516ca8c
YamaMizuTang <b06207004@gmail.com>
2024-01-20 13:54:49 +0800

    use React

---
+++ src/List.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+
+const AddEvent = ({ afterAdded }) => {
+  let [input, setInput] = useState("");
+
+  const submit = async () => {
+    await fetch(`http://127.0.0.1:3001/add?newEvent=${input}`);
+    setInput("");
+    await afterAdded();
+  }
+
+  const inputHandler = (e) => {
+    setInput(e.target.value);
+  }
+
+  return (
+    <div>
+      <input type="text" onChange={inputHandler} value={input}/>
+      <button onClick={submit}>新增</button>
+    </div>
+  );
+};
+
+const RenderList = () => {
+  const [list, setList] = useState([]);
+  
+  const getList = async () => {
+    let fetchPromise = await fetch("http://127.0.0.1:3001/list"); 
+    let todosArray = await fetchPromise.json(); 
+    setList(todosArray);
+  }
+
+  const deleteHandler = async (id) => {
+    await fetch(`http://127.0.0.1:3001/delete?id=${id}`); 
+    getList();
+  }
+
+  useEffect(() => {
+    getList();
+  }, []);
+
+  return (
+    <div>
+      <AddEvent afterAdded={getList} />
+      {list
+        .filter((item) => !item.deleted)
+        .map((item) => {
+          return (
+            <div key={item.id}>
+              <span>{item.name}</span>
+              <button onClick={() => deleteHandler(item.id)}>刪除</button>
+            </div>
+          );
+        })}
+    </div>
+  );
+}
+
+export default RenderList;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import RenderList from './List';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RenderList />
+  </React.StrictMode>
+);
+
